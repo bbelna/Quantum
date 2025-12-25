45d3dc209705f9449e06621b0772d751a326021e
Brandon Belna <bbelna@aol.com>
2025-12-25 15:28:30 -0500

    C runtime: memcpy and memcmp

--- Source/System/User/Runtime/Memory.cpp
+++ Source/System/User/Runtime/Memory.cpp
@@ -17,3 +17,27 @@ extern "C" void* memset(void* dest, int value, unsigned int count) {
 
   return dest;
 }
+
+extern "C" void* memcpy(void* dest, const void* src, unsigned int count) {
+  auto* out = reinterpret_cast<UInt8*>(dest);
+  auto* in = reinterpret_cast<const UInt8*>(src);
+
+  for (unsigned int i = 0; i < count; ++i) {
+    out[i] = in[i];
+  }
+
+  return dest;
+}
+
+extern "C" int memcmp(const void* left, const void* right, unsigned int count) {
+  auto* a = reinterpret_cast<const UInt8*>(left);
+  auto* b = reinterpret_cast<const UInt8*>(right);
+
+  for (unsigned int i = 0; i < count; ++i) {
+    if (a[i] != b[i]) {
+      return static_cast<int>(a[i]) - static_cast<int>(b[i]);
+    }
+  }
+
+  return 0;
+}
