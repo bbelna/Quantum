088032e497a5ea30a2ffbd60739c335512dfe5b4
Brandon Belna <bbelna@aol.com>
2025-12-26 21:59:16 -0500

    Update source file headers in flopy driver

--- Source/System/Drivers/Storage/Floppy/Include/Tests.hpp
+++ Source/System/Drivers/Storage/Floppy/Include/Tests.hpp
@@ -1,9 +1,9 @@
 /**
- * Quantum
- * (c) 2025 Brandon Belna - MIT License
- *
- * System/Drivers/Storage/Floppy/Include/Tests.hpp
- * Floppy driver tests.
+ * @file System/Drivers/Storage/Floppy/Include/Tests.hpp
+ * @brief Floppy driver tests.
+ * @author Brandon Belna <bbelna@aol.com>
+ * @copyright (c) 2025-2026 The Quantum OS Project
+ * SPDX-License-Identifier: MIT
  */
 
 #pragma once
